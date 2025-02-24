77c901579de54f5cb24868e9d7c1c32381ff9190
Jean Victor <jeanvictormachado3@gmail.com>
2025-02-23 22:06:14 -0300

    feat: ensure SignUpController returns 400 if no name is provided

---
+++ src/presentation/controller/signup.ts
@@ -0,0 +1,9 @@
+export class SignupController {
+    handle(httpRequest: unknown): any {
+        const NoResourceFoundStatusCode = 400
+         
+        return {
+            statusCode: NoResourceFoundStatusCode
+        }
+    }
+}
