cdb3c30337525ecd359efda4fd9b2a1ee0d74a33
Jean Victor <jeanvictormachado3@gmail.com>
2025-02-28 23:16:55 -0300

    test: ensure SigupController return 200 if valid data is provided

--- src/presentation/controller/signup/signup.spec.ts
+++ src/presentation/controller/signup/signup.spec.ts
@@ -234,4 +234,27 @@ describe('SignUp Controller', () => {
         expect(httpResponse.statusCode).toBe(500)
         expect(httpResponse.body).toEqual(new ServerError())
     })
+
+    test('Should return 200 if valid data is provided', () => {
+        const {sut } = makeSut();
+        
+        const httpRequest = {
+            body: {
+                email: 'invalid_email@mail.com',
+                name: 'anyname',
+                password: '123456',
+                passwordConfirmation: '123456'
+            }
+        }
+
+        const httpResponse = sut.handle(httpRequest)
+
+        expect(httpResponse.statusCode).toBe(200)
+        expect(httpResponse.body).toEqual({
+            id: 'valid_id',
+            name: 'valid_name',
+            email: 'valid_email@mail.com',
+            password: 'valid_password'
+        })
+    })
 })
